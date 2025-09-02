479154c26704fc6835e6a00e28f09d507d9d5e2a
Pirasannan <dpiraslanka2003@gmail.com>
2025-09-02 10:54:29 +0530

    Completed Signup Validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,16 +2,104 @@
 <?php 
 
 class Users extends Controller {
+    private $userModel; // there was error called undeclared property 
+    
     public function __construct() {
-
+        $this->userModel = $this->model('M_Users');
     }
 
     public function register(){
-        $data = [];
+        if($_SERVER['REQUEST_METHOD'] =='POST'){
+            //form is submitting
+            //validate the data
+            $_POST = array_map('htmlspecialchars', $_POST);
 
-        $this->view('users/v_register',$data);
+            //input data
+            $data=[
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
 
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => '',
+
+            ];
+        
+                //validating each inputs
+
+                //Name validation
+            if(empty($data['name'])){   
+                    $data['name_err'] = 'Please enter a Name';
+            }
+
+            //Email validation
+            if(empty($data['email'])){
+                $data['email_err'] = 'Please enter a valid Email';
+            }
+            else{
+                //email is registered or not
+                if($this->userModel->findUserByEmail($data['email'])){
+                    $data['email_err'] = 'Email is already taken';
+                }
+            }
+
+            //password validation
+            if(empty($data['password'])){ // check for password entered
+                $data['password_err'] = 'Please enter the password';
+            }
+            else if(empty($data['confirm_password'])){ // check for confirm password entered
+                $data['confirm_password_err'] = 'Please confirm the password';
+            }
+            else{
+                if($data['password'] !== $data['confirm_password']){ //chekc both passwords matches
+                    $data['confirm_password_err'] = 'Passwords do not match';
+
+                }
+            }
+        
+
+            //validation completed & No errors , then register users
+            if(empty($data['name_err']) && empty($data['email_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
+                //Hash the password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+                    
+            //Register the user
+                if($this->userModel->register($data)){
+                die('User is Registered');
+                }
+                else{
+                    die('Something went wrong');
+                }
+            }
+            else{
+                //Load view
+                $this->view('users/v_register', $data);
+            }
+        }
+        else {
+            //initial form
+            $data=[
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirm_password' => '',
+
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => '',
+
+            ];
+
+            //Load view
+            $this->view('users/v_register', $data);
+
+        }
     }
+
     public function login(){
         $data = [];
 
--- app/views/users/v_register.php
+++ app/views/users/v_register.php
@@ -8,26 +8,26 @@
                 <center><h1>User Sign up</h1></center>
                 <p><b>Please fill me to register</b></p>
             </div>
-            <form action ="" method="POST">
+            <form action="<?php echo URLROOT?>/Users/register" method="POST">
                 <!-- Name -->
                 <div class="form-input-title">Name</div>
-                <input type="text" name="name" id="name" class = "name ">
-                <span class = "Form-Invalid">ERROR</span>
+                <input type="text" name="name" id="name" class = "name " value = "<?php echo $data['name']?> ">
+                <span class = "Form-Invalid"><?php echo $data['name_err']; ?></span>
 
                 <!-- email -->
                 <div class="form-input-title">Email</div>
-                <input type="text" name="name" id="name" class = "name ">
-                <span class = "Form Invalid"></span>
+                <input type="text" name="email" id="email" class="email" value="<?php echo $data['email']?>">
+                <span class="Form-Invalid"><?php echo $data['email_err']; ?></span>
                   
                 <!-- password -->
-                  <div class="form-input-title">Password</div>
-                <input type="password" name="confirm_password" id="confirm_password" class = "confirm_password">
-                <span class = "Form Invalid"></span>
+                <div class="form-input-title">Password</div>
+                <input type="password" name="password" id="password" class="password" value="<?php echo $data['password']?>">
+                <span class="Form-Invalid"><?php echo $data['password_err']; ?></span>
 
                 <!-- confirm password -->
                 <div class="form-input-title">Confirm password</div>
-                <input type="password" name="name" id="name" class = "name ">
-                <span class = "Form Invalid"></span>
+                <input type="password" name="confirm_password" id="confirm_password" class="confirm_password" value="<?php echo $data['confirm_password']?>">
+                <span class="Form-Invalid"><?php echo $data['confirm_password_err']; ?></span>
                 
                 <br>
                 <input type="submit" value="Register" class="form_btn">

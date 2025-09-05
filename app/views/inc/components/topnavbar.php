dfbb62ac6db54a3eacf37dab1abee8099e691a32
Pirasannan <dpiraslanka2003@gmail.com>
2025-09-06 02:16:55 +0530

    a

--- app/views/inc/components/topnavbar.php
+++ app/views/inc/components/topnavbar.php
@@ -1,6 +1,12 @@
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="<?php echo URLROOT?>/Users/login">Login</a>
-  <a href="<?php echo URLROOT?>/Users/register">Register</a>
-  <!-- <a href="#logout">Logout</a> -->
+<div class="ml-topnav" role="navigation" aria-label="Main Navigation">
+  <div class="ml-nav-left">
+    <a class="ml-logo" href="<?php echo URLROOT; ?>">
+      <span class="ml-logo-text">MediLink</span>
+    </a>
+  </div>
+
+  <div class="ml-nav-actions" role="menubar" aria-label="Primary">
+    <a class="ml-nav-login ml-nav-btn ml-primary" href="<?php echo URLROOT; ?>/Users/login">Login</a>
+    <a class="ml-nav-signup ml-nav-btn ml-primary" href="<?php echo URLROOT; ?>/Users/register">Sign up</a>
+  </div>
 </div>
